c5ed16cdb335734560a4b107dc9ca0054dccb67e
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-20 17:07:39 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// let vector1: string[] = ['a','b','c','d','e','f'];
+// let vector2: string[] = ['a','b','c','d','e','f'];
+
+// vector1 = Number(prompt("Ingrese números de los vectores"));
+
+
+// function sumaVectores (vector1:string,vector2:string):number{
+// return Number(vector1+vector2);
+// };
+// console.log("La suma de los vectores es: ", sumaVectores(vector1,vector2));
+
+// for(let i : number = 0; i<=vector1; i++) {
+
+// }
+
+// let vector1: number[] = [1,2,3,4,5,6];
+let tope: number = 6
+for(let i : number = 0; i>=tope; i++){
+  i = Number(prompt("Ingrese los números"));
+}
