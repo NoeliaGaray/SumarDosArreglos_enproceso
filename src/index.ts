a4e95ba1e6b29c89bdd4a18cb397f925554b7989
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-20 17:56:44 -0300

    PROBANDO COSAS

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@
 
 // vector1 = Number(prompt("Ingrese números de los vectores"));
 
-
 // function sumaVectores (vector1:string,vector2:string):number{
 // return Number(vector1+vector2);
 // };
@@ -14,7 +13,25 @@
 // }
 
 // let vector1: number[] = [1,2,3,4,5,6];
-let tope: number = 6
-for(let i : number = 0; i>=tope; i++){
-  i = Number(prompt("Ingrese los números"));
-}
+
+//probando cosas
+// let tope: number = 6
+// for(let i : number = 1; i>=tope; i++){
+//   i = Number(prompt("Ingrese los números"));
+// }
+
+// let tope: number = 6
+// for (let i = 0; i <= tope; i++) {
+//   i = Number(prompt("Ingrese un número para saber si es par o impar"));
+// console.log("impriimi", i++)
+// }
+
+// let numeros: number[] = [1,2,3,4,5,6];
+
+// for(let i = 1; i <= 6 ; i++){
+//   i += numeros;
+// i = Number(prompt("Ingrese numeros"));
+// let nrovalor: number = Number(prompt("Ingrese numeros"));
+
+// let indice = nrovalor -1;
+// console.log("Numero valor: ", numeros[indice]);
